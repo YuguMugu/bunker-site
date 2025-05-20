3d66d01d241e61e73354734c9803093be82a2f28
YuguMugu <yusufmukhsinov@gmail.com>
2025-05-19 20:02:55 -0500

    compiled.js

--- compiled.js
+++ compiled.js
@@ -1,1 +1,14 @@
-
+// compiled.js (basic placeholder version)
+window.__files = {
+  "files": {
+    "/root.html": `
+      <!DOCTYPE html>
+      <html>
+      <head><title>Bunker Page</title></head>
+      <body>
+        <h2 style='color:white;'>Welcome to your custom Bunker page!</h2>
+      </body>
+      </html>
+    `
+  }
+};
